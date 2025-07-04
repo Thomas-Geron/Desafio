711b1ccd284110a81a3a0ef80d345752871fc005
Thomas Borges Geron <geronthomas123@gmail.com>
2025-07-04 20:42:47 -0300

    feat: add verificador e alerts

--- questao5.js
+++ questao5.js
@@ -1,2 +1,15 @@
 let entrada = prompt("Digite um número inteiro:");
 let numero = parseInt(entrada);
+
+if (!isNaN(numero)) {
+    if (numero % 3 === 0 && numero % 5 === 0) {
+        alert("fizzbuzz");
+    } else if (numero % 3 === 0) {
+        alert("fizz");
+    } else if (numero % 5 === 0) {
+        alert("buzz");
+    } else {
+    }
+} else {
+    alert("Entrada inválida. Por favor, digite um número inteiro.");
+}
