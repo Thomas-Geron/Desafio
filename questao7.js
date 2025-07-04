8ba5085a8587f2135d62cf9f4b89836ae3555da8
Thomas Borges Geron <geronthomas123@gmail.com>
2025-07-04 20:49:00 -0300

    feat: add verificador de palindromo

--- questao7.js
+++ questao7.js
@@ -7,3 +7,12 @@ for (let i = 0; i < frase.length; i++) {
         fraseFormatada += caractere;
     }
 }
+
+let ehPalindromo = true;
+for (let i = 0; i < fraseFormatada.length / 2; i++) {
+    let j = fraseFormatada.length - 1 - i;
+    if (fraseFormatada[i] !== fraseFormatada[j]) {
+        ehPalindromo = false;
+        break;
+    }
+}
