e5bb4ba037520e1ffd6e83af69ac3645c53092f0
Thomas Borges Geron <geronthomas123@gmail.com>
2025-07-04 20:44:15 -0300

    feat: Add function fibonacci

--- questao6.js
+++ questao6.js
@@ -1,2 +1,8 @@
+function fibonacci(n) {
+    if (n === 0) return 0;
+    if (n === 1) return 1;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
 let entrada = prompt("Digite a quantidade de termos da sequência de Fibonacci:");
 let n = parseInt(entrada);
