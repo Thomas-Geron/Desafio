3c1ff4bd8e559c1c29798a5a5e13d5f2e8d00391
Thomas Borges Geron <geronthomas123@gmail.com>
2025-07-04 20:59:01 -0300

    feat: add verificador e contador da ordem

--- questao11.js
+++ questao11.js
@@ -2,3 +2,26 @@ let quantidade = parseInt(prompt("Quantos números você deseja inserir no vetor
 
 if (isNaN(quantidade) || quantidade <= 0) {
     alert("Quantidade inválida. Digite um número inteiro positivo.");
+} else {
+    let vetor = [];
+
+    for (let i = 0; i < quantidade; i++) {
+        let numero = parseInt(prompt(`Digite o ${i + 1}º número:`));
+
+        if (isNaN(numero)) {
+            alert("Entrada inválida. Será considerado 0.");
+            numero = 0;
+        }
+
+        vetor.push(numero);
+    }
+
+    let contadorCrescente = 0;
+    for (let i = 1; i < vetor.length; i++) {
+        if (vetor[i] > vetor[i - 1]) {
+            contadorCrescente++;
+        }
+    }
+
+    alert(`Você inseriu: [${vetor.join(", ")}]\nNúmeros em ordem crescente em relação ao anterior: ${contadorCrescente}`);
+}
