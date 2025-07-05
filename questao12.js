fb047a472618e20b810d101e9755cf0f33572930
Thomas Borges Geron <geronthomas123@gmail.com>
2025-07-04 21:00:50 -0300

    feat: add entrada e impressao de dados

--- questao12.js
+++ questao12.js
@@ -5,3 +5,15 @@ function encontrarElementoUnico(array) {
     }
     return unico;
 }
+
+let entrada = prompt("Digite os números inteiros separados por vírgula (ex: 1,2,3,2,1):");
+
+let partes = entrada.split(',');
+let numeros = [];
+
+for (let i = 0; i < partes.length; i++) {
+    numeros.push(parseInt(partes[i]));
+}
+
+let resultado = encontrarElementoUnico(numeros);
+console.log("O número que aparece apenas uma vez é:", resultado);
