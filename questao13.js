1e6ee3f5db0cb5864f7e16ad14cd63bb2cb16969
Thomas Borges Geron <geronthomas123@gmail.com>
2025-07-04 21:01:56 -0300

    feat: add function buscarPorPosicao

--- questao13.js
+++ questao13.js
@@ -16,3 +16,22 @@ function adicionarJogador() {
     time.push(jogador);
     alert("Jogador adicionado com sucesso!");
 }
+
+function buscarPorPosicao() {
+    let posicaoBusca = prompt("Digite a posição para buscar jogadores:");
+    let encontrados = "";
+    let achou = false;
+
+    for (let i = 0; i < time.length; i++) {
+        if (time[i].posicao.toLowerCase() === posicaoBusca.toLowerCase()) {
+            achou = true;
+            encontrados += `- ${time[i].nome}, ${time[i].idade} anos, Pontuação: ${time[i].pontuacao}\n`;
+        }
+    }
+
+    if (achou) {
+        alert("Jogadores na posição " + posicaoBusca + ":\n" + encontrados);
+    } else {
+        alert("Nenhum jogador encontrado nessa posição.");
+    }
+}
